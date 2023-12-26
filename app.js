f95314852e6cd00113d5a9c0720a9d14a12357d1
baekseojin <seojinbaek100@gmail.com>
2023-12-26 15:29:13 +0900

    STUDY : 3.5 More Events

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 
-function handleTitleClick() {
-  title.style.color = "blue";
-  console.log("title was clicked!");
+function handleH1Click() {
+  h1.style.color = "blue";
+  console.log("h1 was clicked!");
 }
 
 function handleMouseEnter() {
-  title.innerText = "Mouse is here!";
+  h1.innerText = "Mouse is here!";
 }
 
 function handleMouseLeave() {
-  title.innerText = "Mouse is gone!";
+  h1.innerText = "Mouse is gone!";
 }
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+function handleWindowResize() {
+  document.body.style.backgroundColor = "violet";
+}
+
+function handleWindowCopy() {
+  alert("Copy!");
+}
+
+function handleWindowOffline() {
+  alert("Please connect Wifi.");
+}
+
+function handleWindowOnline() {
+  alert("All Goood~");
+}
+
+h1.addEventListener("click", handleH1Click);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
