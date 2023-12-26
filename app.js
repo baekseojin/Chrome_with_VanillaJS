6cbb38522f4f9a60eec0cc397f3094ee44524e8d
baekseojin <seojinbaek100@gmail.com>
2023-12-26 15:12:44 +0900

    STUDY : 3.4 Events part Two

--- app.js
+++ app.js
@@ -5,4 +5,14 @@ function handleTitleClick() {
   console.log("title was clicked!");
 }
 
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone!";
+}
+
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
