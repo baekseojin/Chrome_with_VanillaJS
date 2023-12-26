cbd7fcad21e6727897e4c162fead2ad32f30688b
baekseojin <seojinbaek100@gmail.com>
2023-12-26 09:41:19 +0900

    STUDY : ~ 2.13 Conditionals

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-const calculator = {
-  add: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  times: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  power: function (a, b) {
-    return a ** b;
-  },
-};
+const age = parseInt(prompt("How old are you?"));
 
-const plusResult = calculator.add(5, 2);
-const minusResult = calculator.minus(5, 2);
-const timesResult = calculator.times(5, 2);
-const divideResult = calculator.divide(5, 2);
-const powerResult = calculator.power(5, 2);
+console.log(age);
