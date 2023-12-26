f724939f5f6a7e7a223fec1246087c2662e89e56
baekseojin <seojinbaek100@gmail.com>
2023-12-26 09:13:14 +0900

    STUDY : ~ 2.10 Recap 2

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const player = {
-  name: "seojin",
-  sayHello: function (otherPersonName) {
-    console.log("Hello, " + otherPersonName + "!");
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  times: function (a, b) {
+    console.log(a * b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  power: function (a, b) {
+    console.log(a ** b);
   },
 };
 
-console.log(player.name);
-player.sayHello("seokjin");
+calculator.add(5, 2);
+calculator.minus(5, 2);
+calculator.times(5, 2);
+calculator.divide(5, 2);
+calculator.power(5, 2);
