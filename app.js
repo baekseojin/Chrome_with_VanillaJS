beb4834a76661b0c5ee1ca9f0a5269bf40824459
baekseojin <seojinbaek100@gmail.com>
2023-12-26 15:40:30 +0900

    STUDY : 3.6 CSS in Javascript

--- app.js
+++ app.js
@@ -1,39 +1,14 @@
 const h1 = document.querySelector(".hello h1");
 
 function handleH1Click() {
-  h1.style.color = "blue";
-  console.log("h1 was clicked!");
-}
-
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "violet";
-}
-
-function handleWindowCopy() {
-  alert("Copy!");
-}
-
-function handleWindowOffline() {
-  alert("Please connect Wifi.");
-}
-
-function handleWindowOnline() {
-  alert("All Goood~");
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleH1Click);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
