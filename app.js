f2695c40b876c98edbcfa421fb7fa3bd36c0257b
baekseojin <seojinbaek100@gmail.com>
2023-12-26 11:31:36 +0900

    STUDY : ~ 3.1 HTML in Javascript

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.getElementById("title");
 
-if (isNaN(age) || age < 0) {
-  console.log("Please write a real positive number.");
-} else if (age <= 18) {
-  console.log("You are too young.");
-} else if (age > 18 && age <= 50) {
-  console.log("You can drink.");
-} else if (age > 50 && age <= 80) {
-  console.log("You should exercise.");
-} else if (age > 80) {
-  console.log("You can do whatever you want.");
-}
+title.innerText = "Got you!";
+console.log(title.className);
