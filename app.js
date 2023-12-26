1267ce9484cc98ca0bc323b88c0e18aedcee1876
baekseojin <seojinbaek100@gmail.com>
2023-12-26 12:21:29 +0900

    STUDY : 3.2 Searching For Elements

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const title = document.getElementById("title");
+const title = document.querySelector(".hello h1");
 
-title.innerText = "Got you!";
-console.log(title.className);
+title.innerText = "Hello";
