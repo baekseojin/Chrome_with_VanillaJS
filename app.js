362f36f3b23d68f2bd7e9e0926766bb3eb07ac7c
baekseojin <seojinbaek100@gmail.com>
2023-12-26 09:51:21 +0900

    STUDY : 2.14 Conditionals part Two

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const age = parseInt(prompt("How old are you?"));
 
-console.log(age);
+if (isNaN(age)) {
+  console.log("Please write a number.");
+} else {
+  console.log("Thank you for writing your age.");
+}
